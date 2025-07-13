658c2a948ea3c7f9386a0af6167159c8ee11b053
Avin Motie <amotie@student.ubc.ca>
2025-07-12 21:08:29 -0700

    Added tests for Entry class

--- src/test/model/EntryTest.java
+++ src/test/model/EntryTest.java
@@ -10,7 +10,7 @@ import model.Entry.Mood;
 import java.time.LocalDate;
 
 public class EntryTest {
-    Entry e;
+    private Entry e;
 
     @BeforeEach
     public void setup() {
@@ -18,11 +18,37 @@ public class EntryTest {
     }
 
     @Test
-    void constructorTest() {
+    public void constructorTest() {
         assertEquals(e.getSongName(), "Hello");
         assertEquals(e.getSongArtist(), "Adele");
         assertEquals(e.getDate(), LocalDate.of(2025, 7, 12));
         assertEquals(e.getMood(), Mood.SAD);
         assertTrue(e.getId() > 0);
     }
+
+
+    @Test
+    public void updateSongTitleTest() {
+        e.updateSongTitle(e.getId(), "Someone Like You");
+        assertEquals("Someone Like You", e.getSongName());
+    }
+
+    @Test
+    public void updateSongArtistTest() {
+        e.updateSongArtist(e.getId(), "Lionel Richie");
+        assertEquals("Lionel Richie", e.getSongArtist());
+    }
+
+    @Test
+    public void updateDateTest() {
+        e.updateDate(e.getId(), LocalDate.of(2024, 3, 25));
+        assertEquals(LocalDate.of(2024, 3, 25), e.getDate());
+    }
+
+    @Test
+    public void updateMoodTest() {
+        e.updateMood(e.getId(), Mood.CALM);
+        assertEquals(Mood.CALM, e.getMood());
+    }
+
 }
