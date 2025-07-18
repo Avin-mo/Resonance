28bc6b04d23656ee0c33e28856eda4d197008bfe
Avin Motie <amotie@student.ubc.ca>
2025-07-18 15:40:45 -0700

    Implemented Writable class

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -5,5 +5,5 @@ import org.json.JSONObject;
 public interface Writable {
     // CITATION: this code was taken from CPSC 210 JsonSerializationDemo 
     // return this as a JSON object
-    
+    JSONObject toJson();
 }
