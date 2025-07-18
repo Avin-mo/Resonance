67d19bb9d14c2c558edf6a14c54b67fb3543cb41
Avin Motie <amotie@student.ubc.ca>
2025-07-18 16:06:00 -0700

    Created and spesified JsonWriter class methods

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,45 @@
+package persistence;
+import model.Journal;
+import org.json.JSONObject;
+
+
+import java.io.*;
+
+// CITATION: parts of this code were taken from CPSC 210 JsonSerializationDemo
+
+// Represents a writer that writes JSON representation of workroom to file
+public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of Journal to file
+    public void write(Journal journal) {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        
+    }
+}
