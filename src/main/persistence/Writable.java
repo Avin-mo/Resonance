9ab13dac49996df13dc5438d40806225d6032fe6
Avin Motie <amotie@student.ubc.ca>
2025-07-18 15:40:26 -0700

    Added and specified Writable class

---
+++ src/main/persistence/Writable.java
@@ -0,0 +1,9 @@
+package persistence;
+
+import org.json.JSONObject;
+
+public interface Writable {
+    // CITATION: this code was taken from CPSC 210 JsonSerializationDemo 
+    // return this as a JSON object
+    
+}
