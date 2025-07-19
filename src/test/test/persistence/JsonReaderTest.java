4a03d52190c24f334e5417b5a883cafb803e5075
Avin Motie <amotie@student.ubc.ca>
2025-07-18 17:26:25 -0700

    Added tests for JsonReader class

---
+++ src/test/test/persistence/JsonReaderTest.java
@@ -0,0 +1,52 @@
+package test.persistence;
+
+import model.Entry;
+import model.Journal;
+import persistence.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// CITATION: parts of this code was taken / inspired from CPSC 210 JsonSerializationDemo
+class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            Journal journal = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyJournal() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyJournal.json");
+        try {
+            Journal journal = reader.read();
+            assertEquals(0, journal.getAllEntries().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralJournal() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralJournal.json");
+        try {
+            Journal journal = reader.read();
+            List<Entry> entries = journal.getAllEntries();
+            assertEquals(2, entries.size());
+            checkEntry("Hello", "Adele", LocalDate.of(2025, 7, 12), Entry.Mood.SAD, entries.get(0));
+            checkEntry("good 4 u", "Olivia Rodrigo", LocalDate.of(2024, 9, 24), Entry.Mood.ANGRY, entries.get(1));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+}
